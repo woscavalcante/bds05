e5f08853845655d2ecce669091fd2ddfcd18cec5
Willian Oliveira Siqueira Cavalcante <woscavalcante@outlook.com>
2021-07-11 17:22:34 -0300

    Genre and data seed

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -7,3 +7,14 @@ INSERT INTO tb_role (authority) VALUES ('ROLE_MEMBER');
 INSERT INTO tb_user_role (user_id, role_id) VALUES (1, 1);
 INSERT INTO tb_user_role (user_id, role_id) VALUES (2, 1);
 INSERT INTO tb_user_role (user_id, role_id) VALUES (2, 2);
+
+INSERT INTO tb_genre (name) VALUES ('AÇÃO');
+INSERT INTO tb_genre (name) VALUES ('AVENTURA');
+INSERT INTO tb_genre (name) VALUES ('COMÉDIA');
+INSERT INTO tb_genre (name) VALUES ('DRAMA');
+INSERT INTO tb_genre (name) VALUES ('ROMANCE');
+INSERT INTO tb_genre (name) VALUES ('DOCUMENTÁRIO');
+INSERT INTO tb_genre (name) VALUES ('SUSPENSE');
+INSERT INTO tb_genre (name) VALUES ('POLICIAL');
+INSERT INTO tb_genre (name) VALUES ('TERROR');
+INSERT INTO tb_genre (name) VALUES ('FICÇÃO CIENTÍFICA');
