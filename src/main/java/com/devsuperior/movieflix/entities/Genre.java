116888750f6c7aded106be283b040ca90cab2e80
Willian Oliveira Siqueira Cavalcante <woscavalcante@outlook.com>
2021-07-11 18:39:42 -0300

    Movie, database seed

--- src/main/java/com/devsuperior/movieflix/entities/Genre.java
+++ src/main/java/com/devsuperior/movieflix/entities/Genre.java
@@ -1,11 +1,14 @@
 package com.devsuperior.movieflix.entities;
 
 import java.io.Serializable;
+import java.util.HashSet;
+import java.util.Set;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
 
 @Entity
@@ -17,6 +20,9 @@ public class Genre implements Serializable {
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	private String name;
+	
+	@OneToMany(mappedBy = "genre")
+	private Set<Movie> movies = new HashSet<>();
 	
 	public Genre() {
 		
@@ -42,6 +48,10 @@ public class Genre implements Serializable {
 
 	public void setName(String name) {
 		this.name = name;
+	}
+	
+	public Set<Movie> getMovies() {
+		return movies;
 	}
 
 	@Override
