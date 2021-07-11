812b3352f7949a8d5a6f85e3d6a485098810cafc
Willian Oliveira Siqueira Cavalcante <woscavalcante@outlook.com>
2021-07-11 17:04:07 -0300

    User, Role, fix: application password

--- src/main/java/com/devsuperior/movieflix/MovieflixApplication.java
+++ src/main/java/com/devsuperior/movieflix/MovieflixApplication.java
@@ -2,8 +2,9 @@ package com.devsuperior.movieflix;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {SecurityAutoConfiguration.class })
 public class MovieflixApplication {
 
 	public static void main(String[] args) {
